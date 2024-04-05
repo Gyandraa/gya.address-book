39c7ed90d70a32469678762279de340f5e9cca5e
Gyandraa <gyandranaupal14@gmail.com>
2024-04-06 03:33:44 +0700

    update local storage

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
-class AddressBook {
+class Contacts {
   constructor() {
-    this.contacts = [
-      new Contact(1, "Gyan", "gyan@gmail.com", "+62829238"),
-      new Contact(2, "naufal", "naufal@gmail.com", "+62849382"),
+    const savedContacts = this.loadContacts();
+    this.contacts = savedContacts ? savedContacts : [
+      new Contact("Gyan", "gyan@gmail.com", "+62829238"),
+      new Contact("naufal", "naufal@gmail.com", "+62849382"),
     ];
   }
 
   add(info) {
-    const newContact = new Contact(info.id, info.name, info.email, info.phone);
+    const newContact = new Contact(info.name, info.email, info.phone);
     this.contacts.push(newContact);
+    this.saveContacts(this.contacts);
   }
 
   editAt(index, info) {
-    this.contacts[index] = new Contact(
-      info.id,
-      info.name,
-      info.email,
-      info.phone
-    );
+    this.contacts[index] = new Contact(info.name, info.email, info.phone);
+    this.saveContacts(this.contacts);
   }
 
   deleteAt(index) {
     this.contacts.splice(index, 1);
+    this.saveContacts(this.contacts);
+  }
+
+  saveContacts(contacts) {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }
+
+  loadContacts() {
+    const contacts = localStorage.getItem("contacts");
+    try {
+      return JSON.parse(contacts);
+    } catch (error) {
+      console.error("Failed to load contacts", error);
+      return null;
+    }
+  }
+
+  loadContactById(id) {
+    const contacts = this.loadContacts();
+    if (!contacts) return null;
+    const contact = contacts.find((contact) => contact.id === id);
+    return contact;
   }
 
   renderContact() {
@@ -29,16 +49,16 @@ class AddressBook {
     const searchQuery = document.getElementById("search").value.toLowerCase();
     const filteredContacts = this.contacts.filter(
       (contact) =>
-      contact.name.toLowerCase().includes(searchQuery) ||
-      contact.email.toLowerCase().includes(searchQuery) ||
-      contact.phone.includes(searchQuery)
+        contact.name.toLowerCase().includes(searchQuery) ||
+        contact.email.toLowerCase().includes(searchQuery) ||
+        contact.phone.includes(searchQuery)
     );
     let id = 0;
+
     for (let person of filteredContacts) {
       const newContactEntry = document.createElement("div");
       newContactEntry.classList.add("newCon");
       newContactEntry.innerHTML = `
-                <li>Id: ${person.id}</li>
                 <li>Name: ${person.name}</li>
                 <li>Email: ${person.email}</li>
                 <li>Phone: ${person.phone}</li>
@@ -48,6 +68,7 @@ class AddressBook {
       document.getElementById("contacts").appendChild(newContactEntry);
       id++;
     }
+
     for (let button of document.getElementsByClassName("delete-button")) {
       button.addEventListener("click", () => {
         let index = button.id.split("-")[1];
@@ -55,11 +76,11 @@ class AddressBook {
         this.renderContact();
       });
     }
+
     for (let button of document.getElementsByClassName("edit-button")) {
       button.addEventListener("click", () => {
         let index = button.id.split("-")[1];
         let info = {
-          id: document.getElementById("id").value,
           name: document.getElementById("name").value,
           email: document.getElementById("email").value,
           phone: document.getElementById("phone").value,
@@ -72,38 +93,31 @@ class AddressBook {
   }
 }
 
-// object blueprint to be repeated for each contact
 class Contact {
-  constructor(id, name, email, phone) {
-    this.id = id;
+  constructor(name, email, phone) {
     this.name = name;
     this.email = email;
     this.phone = phone;
   }
 }
 
-// adding a contact
 document.getElementById("add").addEventListener("click", (event) => {
   event.preventDefault();
   let info = {
-    id: document.getElementById("id").value,
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
     phone: document.getElementById("phone").value,
   };
-  newAddressBook.add(info);
-  newAddressBook.renderContact();
+  newContacts.add(info);
+  newContacts.renderContact();
   clearFields();
 });
 
-// clear input fields
 function clearFields() {
-  document.getElementById("id").value = "";
   document.getElementById("name").value = "";
   document.getElementById("email").value = "";
   document.getElementById("phone").value = "";
 }
 
-// creating the new addressbook
-const newAddressBook = new AddressBook();
-newAddressBook.renderContact();
+const newContacts = new Contacts();
+newContacts.renderContact();
